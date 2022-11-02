999e65a9c6924bd710d16deea44c3d3a02b3f114
meryem.sekkat <meryem.sekkat@alithya.com>
2022-11-02 08:37:22 +0100

    manage routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
   resources :posts
-  get 'pages/index'
+  root 'pages#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
