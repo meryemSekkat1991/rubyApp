b2db1bc7873d35ee9e0bb7d5c3673418c6d9f0c2
meryem.sekkat <meryem.sekkat@alithya.com>
2022-11-02 09:34:47 +0100

    manage user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_01_182252) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_02_082155) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,10 +22,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_01_182252) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "email"
     t.text "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
 end
